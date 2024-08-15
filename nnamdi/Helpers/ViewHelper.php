776c8fbad130750a93d7748e90b6911f2aeb413c
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2024-08-15 10:44:46 -0400

    render custom view

--- nnamdi/Helpers/ViewHelper.php
+++ nnamdi/Helpers/ViewHelper.php
@@ -4,15 +4,22 @@ namespace EcommerceGroup10\Cakery\Helpers;
 
 class ViewHelper
 {
+    private $viewRoot = "bhumi";
     public static function renderView($view, $data = [])
     {
         extract($data);
         ob_start();
-        $viewPath = APP_ROOT . "/bhumi/Views/{$view}.php";
+        $viewPath = APP_ROOT . "/{$this->viewRoot}/Views/{$view}.php";
         if (!file_exists($viewPath)) {
             throw new \Exception("View file not found: {$viewPath}");
         }
         include $viewPath;
         return ob_get_clean();
     }
+    
+    public static function renderCustomView($viewRoot, $view, $data){
+        $this->viewRoot = $viewRoot;
+        return $this->renderView($view, $data);
+    }
 }
+
