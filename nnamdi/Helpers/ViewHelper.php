057fc7ecebebdd18216c86a68295fb96b522f18f
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2024-08-15 10:49:45 -0400

    render custom view fixed

--- nnamdi/Helpers/ViewHelper.php
+++ nnamdi/Helpers/ViewHelper.php
@@ -1,25 +1,26 @@
 <?php
-
 namespace EcommerceGroup10\Cakery\Helpers;
 
 class ViewHelper
 {
-    private $viewRoot = "bhumi";
-    public static function renderView($view, $data = [])
+    private static $defaultViewRoot = "bhumi";
+
+    public static function renderView($view, $data = [], $viewRoot = null)
     {
+        $viewRoot = $viewRoot ?? self::$defaultViewRoot;
         extract($data);
         ob_start();
-        $viewPath = APP_ROOT . "/{$this->viewRoot}/Views/{$view}.php";
+        $viewPath = APP_ROOT . "/{$viewRoot}/Views/{$view}.php";
         if (!file_exists($viewPath)) {
             throw new \Exception("View file not found: {$viewPath}");
         }
         include $viewPath;
         return ob_get_clean();
     }
-    
-    public static function renderCustomView($viewRoot, $view, $data){
-        $this->viewRoot = $viewRoot;
-        return $this->renderView($view, $data);
+
+    public static function renderCustomView($viewRoot, $view, $data = [])
+    {
+        return self::renderView($view, $data, $viewRoot);
     }
 }
 
